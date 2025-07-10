e23b653442b1f0cd82c013a37995009108d707ba
bezark <jdb459@nyu.edu>
2025-07-10 19:02:49 -0400

    Create pi-startup.sh

---
+++ pi-startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+sleep 10
+
+
+cd /home/monster/Documents/Monsters-Are-Attacking-the-City
+
+git pull origin main
+
+./home/monster/Documents/Godot/Godot_v4.4.1.arm64 /home/monster/Documents/Monsters-Are-Attacking-the-City/monsters-attacking/
