809c6522205fad67c42c4f14f99cb0294030d4e2
bezark <jdb459@nyu.edu>
2025-07-10 19:11:45 -0400

    Update pi-startup.sh

--- pi-startup.sh
+++ pi-startup.sh
@@ -8,4 +8,5 @@ cd /home/monster/Documents/Monsters-Are-Attacking-the-City
 
 git pull origin main
 
-/home/monster/Documents/Godot/Godot_v4.4.1.arm64 /home/monster/Documents/Monsters-Are-Attacking-the-City/monsters-attacking/
+/home/monster/Documents/Godot/Godot_v4.4.1.arm64 --import --path /home/monster/Documents/Monsters-Are-Attacking-the-City/monsters-attacking/
+/home/monster/Documents/Godot/Godot_v4.4.1.arm64 --path /home/monster/Documents/Monsters-Are-Attacking-the-City/monsters-attacking/
