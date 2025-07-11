ca639cded473698f37b36b4af4fef836c3e3de86
bezark <jdb459@nyu.edu>
2025-07-11 16:31:00 -0400

    Update simple_recorder.gd

--- monsters-attacking/simple_recorder.gd
+++ monsters-attacking/simple_recorder.gd
@@ -11,21 +11,13 @@ var recording_path: String = "res://recording.ogv"
 func start_recording():
 	var output_path = ProjectSettings.globalize_path(recording_path)
 	
-	var args = ["ffmpeg",
-		"-device", "/dev/dri/card0",
-		"-f", "kmsgrab",
-		"-i", "-",
-		"-f", "alsa",
-		"-i", "plughw:3,0",    # Use plughw instead of hw
- 		"-vf", "hwdownload,format=bgr0",
-		"-c:a", "libvorbis",
-		"-q:a", "4",
-		"-y",
-		output_path
+	var args = [
+		"--audio=Andrea_PureAudio,0",
+		"--file=output.mp4"
 	]
 	# No more sudo needed!
 	
-	ffmpeg_pid = OS.create_process("sudo", args)
+	ffmpeg_pid = OS.create_process("wf-recorder", args)
 	print("Started recording with PID: ", ffmpeg_pid)
 
 func stop_recording():
@@ -35,6 +27,10 @@ func stop_recording():
 		await get_tree().create_timer(1.0).timeout
 		
 		ffmpeg_pid = -1
+		OS.execute("ffmpeg" ,[
+			"-i", "output.mp4", "-c:v", "libtheora", "-q:v", "4", "-c:a", "libvorbis", "-q:a", "3", "output.ogv"
+		]
+)
 		print("Recording stopped")
 		play_video()
 
@@ -58,7 +54,7 @@ func _on_record_toggled(toggled_on: bool) -> void:
 		
 func play_video():
 	$CenterContainer/VBoxContainer/WebcamTexture.hide()
-	video.stream.file=recording_path
+	video.stream.file="output.ogv"
 	video.show()
 	video.play()
 
