731d32574ed77a313aaeeee877e8c6355dbf0865
bezark <jdb459@nyu.edu>
2025-07-15 19:19:19 -0400

    Update pi-startup.sh

--- pi-startup.sh
+++ pi-startup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-
 sleep 5
-
-
 cd /home/monster/Documents/Monsters-Are-Attacking-the-City
 
-git pull origin main
+# Force repository to match remote exactly
+git fetch origin
+git reset --hard origin/main
+git clean -fd  # Remove untracked files/directories
 
 /home/monster/Documents/Godot/Godot_v4.4.1.arm64 --import --path /home/monster/Documents/Monsters-Are-Attacking-the-City/monsters-attacking/
 /home/monster/Documents/Godot/Godot_v4.4.1.arm64 --path /home/monster/Documents/Monsters-Are-Attacking-the-City/monsters-attacking/
