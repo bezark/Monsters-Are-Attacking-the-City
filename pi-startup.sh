f92d6f9404d4401568091ddcd086d029bbe24b91
bezark <jdb459@nyu.edu>
2025-07-17 12:18:26 -0400

    Update pi-startup.sh

--- pi-startup.sh
+++ pi-startup.sh
@@ -2,10 +2,11 @@
 sleep 5
 cd /home/monster/Documents/Monsters-Are-Attacking-the-City
 
-# Force repository to match remote exactly
-git fetch origin
-git reset --hard origin/main
-git clean -fd  # Remove untracked files/directories
+# Force repository to match remote exactly (but don't fail if no internet)
+git fetch origin || true
+git reset --hard origin/main || true
+git clean -fd || true
 
+# These will always run regardless of git success/failure
 /home/monster/Documents/Godot/Godot_v4.4.1.arm64 --import --path /home/monster/Documents/Monsters-Are-Attacking-the-City/monsters-attacking/
 /home/monster/Documents/Godot/Godot_v4.4.1.arm64 --path /home/monster/Documents/Monsters-Are-Attacking-the-City/monsters-attacking/
