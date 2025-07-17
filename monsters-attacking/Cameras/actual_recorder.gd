9f65b2e4d6f61cf49cfc135b1c7b4eae9dfe8591
bezark <jdb459@nyu.edu>
2025-07-17 10:44:51 -0400

    tests

--- monsters-attacking/Cameras/actual_recorder.gd
+++ monsters-attacking/Cameras/actual_recorder.gd
@@ -30,59 +30,92 @@ func _input(event: InputEvent) -> void:
 			
 
 func start_recording():
+	
 	state = "recording"
 	var output_path = ProjectSettings.globalize_path(base_path)
 	current_video = Time.get_datetime_string_from_system()
 	full_video_path = str(output_path,current_video)
 	print(full_video_path)
-	var args = [
-		"-v", "verbose",
-		
-		# Audio input FIRST (helps with sync)
-		"-f", "alsa",
-		"-thread_queue_size", "2048",  # Increase buffer
-		"-ar", "44100",  # Set sample rate explicitly
-		"-ac", "1",  # Mono
-		"-i", "hw:CARD=PureAudio,DEV=0",
-		
-		# Video input SECOND
-		"-f", "x11grab", 
-		"-r", "30",
-		"-video_size", "720x480",  # Use -video_size instead of -s
-		"-i", ":0.0",
-		
-		# Sync and timing options
-		"-use_wallclock_as_timestamps", "1",  # Better timestamp handling
-		"-async", "1",  # Audio sync
-		"-vsync", "1",  # Video sync
-		
-		# Encoding options
-		"-c:v", "libtheora", 
-		"-q:v", "10",
-		"-c:a", "libvorbis",
-		"-q:a", "5",  # Audio quality
-		
-		# Output
-		str(full_video_path, ".ogv")
-	]
+	var args
+	var system = OS.get_distribution_name()
+	print(system)
+	if system == "cpe:/o:nixos:nixos:25.11":
+		args = [
+			"-f", "pulse",
+			"-i", "default",
+			"-f", "lavfi", 
+			"-i", "color=c=black:s=720x480:r=30",
+			"-c:v", "libtheora",
+			"-q:v", "10",
+			"-c:a", "libvorbis",
+			"-q:a", "5",
+			"-shortest",
+			str(full_video_path, ".ogv")
+		]
+	else:
+		args = [
+			"-v", "verbose",
+			
+			# Audio input FIRST (helps with sync)
+			"-f", "alsa",
+			"-thread_queue_size", "2048",  # Increase buffer
+			"-ar", "44100",  # Set sample rate explicitly
+			"-ac", "1",  # Mono
+			"-i", "hw:CARD=PureAudio,DEV=0",
+			
+			# Video input SECOND
+			"-f", "x11grab", 
+			"-r", "30",
+			"-video_size", "720x480",  # Use -video_size instead of -s
+			"-i", ":0.0",
+			
+			# Sync and timing options
+			"-use_wallclock_as_timestamps", "1",  # Better timestamp handling
+			"-async", "1",  # Audio sync
+			"-vsync", "1",  # Video sync
+			
+			# Encoding options
+			"-c:v", "libtheora", 
+			"-q:v", "10",
+			"-c:a", "libvorbis",
+			"-q:a", "5",  # Audio quality
+			
+			# Output
+			str(full_video_path, ".ogv")
+		]
 	
 	ffmpeg_pid = OS.create_process("ffmpeg", args)
 	print("Started recording with PID: ", ffmpeg_pid)
 
-
 func stop_recording():
 	if ffmpeg_pid != -1:
-		OS.execute("kill", ["-INT", str(ffmpeg_pid)])
-		
-		await get_tree().create_timer(1.0).timeout
-		
-		#ffmpeg_pid = -1
-		#OS.execute("ffmpeg" ,[
-			#"-i", str(full_video_path,".mkv"),  "-c:v", "libtheora", "-q:v", "6", "-c:a", "libvorbis", "-q:a", "3", str(full_video_path,".ogv")
-		#]
-		#)
+		# Send 'q' to ffmpeg stdin instead of kill -INT
+		# This allows ffmpeg to properly flush buffers
+		OS.execute("echo", ["q"], [], true, true)
+
+		# Give more time for buffer flushing
+		await get_tree().create_timer(2.0).timeout
+
+		# If still running, then use kill
+		if OS.is_process_running(ffmpeg_pid):
+			OS.execute("kill", ["-INT", str(ffmpeg_pid)])
+			await get_tree().create_timer(1.0).timeout
+
 		print("Recording stopped")
 		play_video()
+#func stop_recording():
+	#if ffmpeg_pid != -1:
+		#OS.execute("kill", ["-INT", str(ffmpeg_pid)])
+		#
+		#await get_tree().create_timer(1.0).timeout
+		#
+		##ffmpeg_pid = -1
+		##OS.execute("ffmpeg" ,[
+			##"-i", str(full_video_path,".mkv"),  "-c:v", "libtheora", "-q:v", "6", "-c:a", "libvorbis", "-q:a", "3", str(full_video_path,".ogv")
+		##]
+		##)
+		#print("Recording stopped")
+		#play_video()
 
 func _exit_tree():
 	# Clean up if still recording
